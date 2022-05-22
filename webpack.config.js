33c6e8df10e7472cb06272a31a3ccd22ab2e9fd7
이순현 <sunwl123@naver.com>
2022-05-22 18:19:01 +0900

    ok

--- webpack.config.js
+++ webpack.config.js
@@ -61,21 +61,13 @@ module.exports = {
         filename: 'bundle.js',
         publicPath: '/dist'
     },
- 
-    devServer: {
-        proxy: {
-            '/api': {
-            target: 'https://hnm-express-project.herokuapp.com/',
-            changeOrigin: true,
-            }
-        },
-        static: {
-            directory: path.join(__dirname, 'public')
-        },
-        compress: true,
-        port: 3000,
-        hot: true,
-        historyApiFallback: true 
+    proxy: {
+        '/api': {
+        target: 'https://hnm-express-project.herokuapp.com/',
+        changeOrigin: true,
+        }
     },
- 
+    static: {
+        directory: path.join(__dirname, 'public')
+    },
 }
