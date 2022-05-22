7346347b054834134b5f5a3e4b5cf351fa5298f6
이순현 <sunwl123@naver.com>
2022-05-23 02:38:45 +0900

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
