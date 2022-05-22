8849545e90915f6e9b72b0dcdd27204688073993
이순현 <sunwl123@naver.com>
2022-05-23 02:38:17 +0900

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,20 @@ import Navbar from "./component/Navbar";
 import ProductAll from "./page/ProductAll";
 import Login from "./page/Login";
 import PrivateRoute from "./Route/PrivateRoute";
+import axios from "axios";
 
 function App() {
+  async function check() {
+    try{
+        const res = await axios.get('/api/');
+        console.log(res.data.user);
+    } catch(e){
+        console.error(e.message)
+    }
+  }
+  useEffect(() => {
+      check();
+  },[])
   let [authenticate, setAuthenticate] = useState(false);
   return (
     <div>
