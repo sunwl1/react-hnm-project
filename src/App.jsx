854548616d48865501e6890577b2b3598896041f
이순현 <sunwl123@naver.com>
2022-05-23 02:33:21 +0900

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
---
+++ webpack.config.js
@@ -0,0 +1,81 @@
+const path = require('path')  
+const webpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+ 
+module.exports = {
+ 
+    name: 'react-project',
+    
+    mode: 'development',
+ 
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    
+    entry: {
+        app: ['./src/index.jsx']
+    },
+ 
+    module: {
+        rules: [{
+            test: /\.jsx?/,
+            loader: 'babel-loader', 
+            options: {
+                presets: [
+                    ['@babel/preset-env', {  
+                        targets: {
+                            browsers: [  
+                                'last 2 chrome versions',
+                                '> 5% in KR'
+                            ]
+                        },
+                        debug: true
+                    }],  
+                    '@babel/preset-react'  
+                ],
+                plugins: [
+                    'react-refresh/babel'
+                ]
+            }
+        },
+        {
+            test: /\.css$/,
+            use: [{
+                loader: MiniCssExtractPlugin.loader,
+                options: {
+                    //
+                }
+            },
+            'css-loader'
+            ]
+        }]
+    },
+ 
+    plugins: [
+        new webpackPlugin(),
+        new MiniCssExtractPlugin({ filename: 'style.css' }) 
+    ],
+ 
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/dist'
+    },
+ 
+    devServer: {
+        proxy: {
+            '/api': {
+            target: 'https://hnm-express-project.herokuapp.com/',
+            changeOrigin: true,
+            }
+        },
+        static: {
+            directory: path.join(__dirname, 'public')
+        },
+        compress: true,
+        port: 3000,
+        hot: true,
+        historyApiFallback: true 
+    },
+ 
+}
