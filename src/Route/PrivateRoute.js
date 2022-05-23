5cac9050c4376e144238366877506c9c215069d3
sunwl1 <sunwl123@naver.com>
2022-05-23 12:18:08 +0900

    ss
    ss

--- src/Route/PrivateRoute.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Navigate } from "react-router-dom";
-import ProductDetail from "../page/ProductDetail";
-
-const PrivateRoute = ({ authenticate }) => {
-  return authenticate ? <ProductDetail /> : <Navigate to="/login" />;
-};
-
-export default PrivateRoute;
